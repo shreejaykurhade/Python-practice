f608a62ef0c916f0dd5b5786aabd3573c01f3e36
Shreejay Kurhade <93570022+shreejaykurhade@users.noreply.github.com>
2023-12-27 13:22:02 +0530

    Delete bandnamegenerator.py

--- bandnamegenerator.py
+++
@@ -1,5 +0,0 @@
-print("Welcome to the Band name generator , ")
-city = input("Enter the name pf your city : ")
-pet = input("Enter name of your pet : ")
-
-print("The name of your band can be " + " " + city + " " + pet) 
